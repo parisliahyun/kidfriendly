4aff1cad53640708a2482630e0f093c38ff825d7
My Name <parisliahyun@gmail.com>
2013-12-10 12:32:12 -0500

    added image url to account  update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,14 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :image_url)}
+
+    devise_parameter_sanitizer.for(:sign_up) << :image_url
     devise_parameter_sanitizer.for(:sign_up) << :name
-    devise_parameter_sanitizer.for(:account_update) << :name
+    # devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :image_url, :password)}
+    devise_parameter_sanitizer.for(:account_update) << :name 
+    devise_parameter_sanitizer.for(:account_update) << :image_url
   end
 end
+
+
