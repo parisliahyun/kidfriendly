bfd93d50628705b9c4ade07258ce1e52aac691e8
My Name <parisliahyun@gmail.com>
2013-12-15 21:36:25 -0500

    above the app name

--- config/application.rb
+++ config/application.rb
@@ -10,10 +10,11 @@ require "sprockets/railtie"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
-config.assets.initialize_on_precompile = false
+
 module KidFriendly
+    config.assets.initialize_on_precompile = false
   class Application < Rails::Application
-    
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
