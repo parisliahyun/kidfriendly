e4cd88bf97c073d458069ca224dd2a1afce550e9
My Name <parisliahyun@gmail.com>
2013-12-15 22:21:18 -0500

    stuff

--- config/application.rb
+++ config/application.rb
@@ -27,6 +27,7 @@ module KidFriendly
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     # I18n.enforce_available_locales = false
+    I18n.enforce_available_locales = false
     config.assets.initialize_on_precompile = false
     # config.paths['app/views'] << "app/views/devise"
 
