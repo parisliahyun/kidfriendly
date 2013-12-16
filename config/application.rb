298b297e92f74e69b70b40494c88d3ecd3d11a27
My Name <parisliahyun@gmail.com>
2013-12-15 21:21:41 -0500

    commented out the line as well as I18n.enforce_available_locales = false

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ require "sprockets/railtie"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
-
+# config.assets.initialize_on_precompile = false
 module KidFriendly
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
@@ -24,9 +24,9 @@ module KidFriendly
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    I18n.enforce_available_locales = false
+    # I18n.enforce_available_locales = false
     
     # config.paths['app/views'] << "app/views/devise"
-    # config.assets.initialize_on_precompile = false
+
   end
 end
