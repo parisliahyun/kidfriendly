cea3fa256884456fea1f1e3c5ba006a0c28e13bc
My Name <parisliahyun@gmail.com>
2013-12-16 12:59:09 -0500

    edits to search form post with :q

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -6,10 +6,12 @@ class SearchesController < ApplicationController
   end
 
   def create
-    @appointment = Appointment.create(appointment_params)
-    @schedules = Schedule.all
+    
     @activities = Activity.all
     @activity = Activity.find(params[:id])
+    @selected_tag = Activity.find(params[:tag_list])
+    @appointment = Appointment.create(appointment_params)
+    @schedules = Schedule.all
     @appointment.activity_id = @activity.activity_id
     @appointment.client_id = current_user.client_id
     @appointment.vendor_id = @activity.vendor_id
@@ -17,19 +19,16 @@ class SearchesController < ApplicationController
     @users = User.all 
     @user = User.find(params[:id])
     @schedule = Schedule.find(params[:id])
-    @schedules.each do |schedule|
-    @matching_schedules = []
-      if params[:from_date] == schedule.from_date
-        @matching_schedules << schedule
-      end  
-    end  
-    # if @matching_schedules != nil
-    if params[":from_date"] == "2013-12-31"
+    # @schedules.each do |schedule|
+    # @matching_schedules = []
+    #   if params[:from_date] == schedule.from_date
+    #     @matching_schedules << schedule
+    #   end  
+    # end  
+    # if params[":from_date"] == "2013-12-31"
       render :results
-    else
-      render "no activities are available for this time."
-    end
-
+      # binding.pry
+      # render "no activities are available for this time."
   end
 
   def index 
