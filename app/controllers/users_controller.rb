aa33b03fc234a25abf20cdf9f19b6ec78986714b
My Name <parisliahyun@gmail.com>
2013-12-17 13:49:33 -0500

    fixed email bug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ def show
   @comments = @commentable.comments
   @comment = Comment.new
   @activities = Activity.where(vendor_id: [current_user.id])
-
+  @basic_profile = get_basic_profile
     # display history of booked activities:
     # @appointments = Appointment.where(vendor_id: [current_user.id])
     # loop through appointments and:
@@ -57,5 +57,22 @@ def update_with_password(params, *options)
     super
   end
 end
+
+  def get_basic_profile
+    bprofile = BasicProfile.find_by_user_id(current_user.id)
+    if bprofile.nil?
+      client = get_client
+      profile = client.profile(:fields => ["first-name", "last-name", "maiden-name", "formatted-name" ,:headline, :location, :industry, :summary, :specialties, "picture-url", "public-profile-url"])
+
+      basic_profile = profile.to_hash
+      basic_profile[:location] = basic_profile["location"]["name"]
+      new_basic_profile = BasicProfile.new(basic_profile)
+      new_basic_profile.user = current_user
+      new_basic_profile.save 
+      new_basic_profile
+    else
+      bprofile
+    end
+  end
 
 end  
