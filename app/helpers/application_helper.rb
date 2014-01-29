cc1f27d16323b8bf84151cc9e30099aea89a3af6
My Name <parisliahyun@gmail.com>
2014-01-28 16:37:41 -0800

    background image

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
 
   def random_background
-    ["http://www.jetblue.com/img/vacations/destination/Cancun-960-x-420.jpg", "http://24.media.tumblr.com/960c087c26cfabc839ec565ebacac08e/tumblr_mpimtdGFvm1rsielzo1_1280.jpg", "http://www.parade.com/wp-content/uploads/2013/05/road-trip-ftr.jpg", "http://i.huffpost.com/gen/1405567/thumbs/o-MILLENNIAL-WOMEN-facebook.jpg", "http://blogs.tennessean.com/business/files/2013/02/Seed-Money.jpg"].sample
+    ["http://i1292.photobucket.com/albums/b561/parisliahyun/01501c32-b1aa-44a9-a10e-e04659cdda05_zps942f9b13.jpg"].sample
   end
 end
